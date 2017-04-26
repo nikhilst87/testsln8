2384283275537e306d8af1e22f8285ab28476565
NikhilST <nikhilsthakare29@gmail.com>
2017-04-26 11:36:08 +0100

    commit on develop branch

--- testsln8/IService1.cs
+++ testsln8/IService1.cs
@@ -7,7 +7,7 @@ using System.ServiceModel.Web;
 using System.Text;
 
 namespace testsln8
-{
+{ //comment on develop branch
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
